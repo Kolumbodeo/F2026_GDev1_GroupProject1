be28a5997d8c5187ea4f37bba29fd71d04ca10ba
at909524 <twymanacj@outlook.com>
2026-09-17 01:23:51 -0400

    Mockup 1
    Created basic mockup of 1st level for progress report.

---
+++ GameDev1_Group_Project/Assets/Main Folder/Scripts/Player.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public PlayerInput playerInput;
+
+    [Header("Movement Variables")]
+    public float speed;
+    public float jumpForce;
+
+    public int facingDirection = 1;
+
+    public Vector2 moveInput;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+
+    void Update()
+    {
+        CheckGrounded();
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        float targetSpeed = moveInput.x * speed;
+        rb.linearVelocity =new Vector2(targetSpeed, rb.linearVelocity.y);
+    }
+
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void Flip()
+    {
+        if (moveInput.x > 0.1f)
+        {
+            facingDirection = 1;
+        }
+        else if (moveInput.x < -0.1f)
+        {
+            facingDirection = -1;
+        }
+    transform.localScale = new Vector3(facingDirection, 1, 1);
+    }
+
+    public void OnMove (InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump (InputValue value)
+    {
+        if(value.isPressed && isGrounded)
+       { 
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+       }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+}
